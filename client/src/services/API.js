3967d525105582be6bdeca39e0634c45c46669ab
HarshdeepSingh2609 <harshpadam2004@gmail.com>
2025-06-30 21:23:37 +0530

    Configured frontend for Vercel deployment

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,10 +1,26 @@
+// import axios from "axios";
+
+// const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
+
+// API.interceptors.request.use((req) => {
+//   if (localStorage.getItem("token")) {
+//     req.headers.Authorization = `Bearer ${localStorage.getItem("token")} `;
+//   }
+//   return req;
+// });
+
+// export default API;
+// client/src/services/API.js
+
 import axios from "axios";
 
 const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
 
+// Intercept every request to include token (if exists)
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem("token")) {
-    req.headers.Authorization = `Bearer ${localStorage.getItem("token")} `;
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
   }
   return req;
 });
